06d4997f415237c91beea146f27b3a9d578e074b
dawidplatek <dawidplatek.eu.pl@outlook.com>
2024-10-13 21:36:12 +0200

    first commit

---
+++ test.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test</title>
+</head>
+<body>
+    <?php
+        echo('Hello world1');
+        // echo('Hello world2');
+        echo('Hello world3');
+        /* echo('Hello world4');
+        echo('Hello world5');*/
+        echo('Hello world6');
+    ?>
+</html>
