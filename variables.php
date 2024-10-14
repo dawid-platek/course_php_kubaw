c2cbbdc40ae777126b9e7971fa7cd62d677cae9d
dawidplatek <dawidplatek.eu.pl@outlook.com>
2024-10-14 23:53:04 +0200

    Section 2 - variable declaration in PHP

---
+++ variables.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+    $num = 100; //deklaracja zmiennej
+    $num = 123; //zmiana wartości zmiennej
+
+    echo ("Wartość w zmiennej num to:  $num");
+    ?>
+    
+</body>
+</html>
