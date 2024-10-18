7172cc0748884cfd14f9e767e3d20d6444c41eca
dawidplatek <dawidplatek.eu.pl@gmail.com>
2024-10-18 21:53:34 +0200

    Section 2 - correct identifiers - task

---
+++ challenges/variables.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+$titleMail = "Testowy mail";
+$senderMail = "sender@test.pl";
+$receiverMail = "receiver@test.pl";
+$message = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.";
+$date = "2024-01-01";
+
+
+
+?>
+
+
+</body>
+</html>
